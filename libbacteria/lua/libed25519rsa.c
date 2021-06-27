413838886cd4ddf363c0274e4dbc260706c73185
wipedlife <potatolivingstonei2p@gmail.com>
2021-06-27 09:06:50 +0300

    libed25519rsa pre init

--- libbacteria/lua/libed25519rsa.c
+++ libbacteria/lua/libed25519rsa.c
@@ -1,4 +1,77 @@
 #include"libed25519rsa.h"
+
+int luaopen_libed25519rsa(lua_State *L){
+  luaL_openlib(L, "ed25519rsa", libed25519rsa, 0);
+  return 1;
+}
+/*
+struct aKeyPair {
+  char *pubKey, *privKey;
+  size_t pubKeyLen, privKeyLen;
+  EVP_PKEY *pkey;
+  enum aTypes type;
+};
+*/
+
+#define GETRET(type)\
+	 size_t nbytes = sizeof(char *)*2 +\
+                  sizeof(size_t)*(rt.pubKeyLen-1) + sizeof(size_t)*(rt.privKeyLen-1) \
+		+ sizeof(EVP_PKEY *) + sizeof(enum aTypes); \
+  	struct aKeyPair *ret = (struct aKeyPair *)lua_newuserdata(L, nbytes);\
+	ret->pubKey = rt.pubKey;\
+	ret->privKey = rt.privKey;\
+	ret->pubKeyLen = rt.pubKeyLen;\
+	ret->privKeyLen = rt.privKeyLen;\
+	ret->pkey = rt.pkey;\
+
+//	ret->type=(enum aTypes)ed25519;
+
+INITLUAFUNC(generateKeysEd25519){
+	char * filepath = NULL;
+	FILE * exitFile=NULL;
+	if( lua_isstring(L, 1) ){
+		filepath = (char*)luaL_checkstring(L, 1);
+	}
+	if(filepath != NULL){
+		exitFile = fopen(filepath,"wb");
+	}
+	struct aKeyPair rt = generateKeysEd25519(exitFile);
+	if(exitFile!=NULL) fclose(exitFile);
+	GETRET(ed25519);
+	return 1;
+}
+
+INITLUAFUNC(generateKeysRSA){
+	char * filepath = NULL;
+	FILE * exitFile=NULL;
+	int kBits = 8192;
+	int primes = 3;
+	if( lua_isnumber(L, 1) ) kBits = lua_tonumber(L,1);	
+	if( lua_isnumber(L, 2) ) primes = lua_tonumber(L,2);	
+	if( lua_isstring(L, 3) ){
+		filepath = (char*)luaL_checkstring(L, 1);
+	}
+	if(filepath != NULL){
+		exitFile = fopen(filepath,"wb");
+	}
+	struct aKeyPair rt = generateKeysRSA(kBits,primes,exitFile);
+	if(exitFile!=NULL) fclose(exitFile);
+	GETRET(aRSA);
+	return 1;
+}
+
+INITLUAFUNC(initPrivKey){
+
+}
+
+INITLUAFUNC(singIt){
+
+}
+
+INITLUAFUNC(verifyIt){
+
+}
+
 /*
 #include"rsa_ed25519.h"
 int main(void) {
--- libbacteria/lua/libed25519rsa.h
+++ libbacteria/lua/libed25519rsa.h
@@ -1,4 +1,25 @@
+#pragma once
 #include"lua.h"
 #include"encdec/rsa_ed25519.h"
 
+INITLUAFUNC(generateKeysEd25519);
+INITLUAFUNC(generateKeysRSA);
+INITLUAFUNC(initPrivKey);
+INITLUAFUNC(singIt);
+INITLUAFUNC(verifyIt);
 
+//INITLUAFUNC(Base64Decode);
+//INITLUAFUNC(Base64Encode);
+
+static const struct luaL_reg libed25519rsa [] = {
+//	LUAPAIR(getAESData_len)
+LUAPAIR(generateKeysEd25519)
+LUAPAIR(generateKeysRSA)
+LUAPAIR(initPrivKey)
+LUAPAIR(singIt)
+LUAPAIR(verifyIt)
+//LUAPAIR(freeKeys)
+//LUAPAIR(getPrivPubRaw)
+};
+
+int luaopen_libed25519rsa (lua_State *L);
